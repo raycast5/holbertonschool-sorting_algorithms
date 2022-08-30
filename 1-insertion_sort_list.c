6b0b37b737e5640c970906eec9ab9ec8563c2fa9
raycast5 <ray.j.colon@gmail.com>
2022-08-30 17:11:36 +0000

    Changed order of loop conditionals

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,35 +2,35 @@
 
 /**
  * insertion_sort_list - sorts a doubly linked list in ascending order
- * @list - list to be sorted
+ * @list: list to be sorted
  * Return: Nothing
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *key;
+	listint_t *next, *key;
 
 	if (!*list || !list)
 		return;
 
-	current = *list;
-	while (current->next)
+	key = (*list)->next;
+	while (key)
 	{
-		current = current->next;
-		key = current;
-		while (key->n < key->prev->n && key->prev)
+		next = key->next;
+		while (key->prev && key->n < key->prev->n)
 		{
 			key->prev->next = key->next;
-			if(key->next)
+			if (key->next)
 				key->next->prev = key->prev;
 			key->next = key->prev;
-			key->prev = key->prev->prev;
-			if(key->prev)
-				key->prev->next = key;
+			key->prev = key->next->prev;
 			key->next->prev = key;
-			if(!key->prev)
+			if (key->prev)
+				key->prev->next = key;
+			else
 				*list = key;
 			print_list(*list);
 		}
+		key = next;
 	}
 }

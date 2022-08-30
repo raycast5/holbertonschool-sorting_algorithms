41f668a2711da32c68f72d4728a0d0c0ab39cf55
raycast5 <ray.j.colon@gmail.com>
2022-08-30 17:15:55 +0000

    Fixed for empty list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *next, *key;
 
-	if (!*list || !list || !list->next)
+	if (!*list || !list || !(*list)->next)
 		return;
 
 	key = (*list)->next;
